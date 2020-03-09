140f4dbe423d0128532987a285d99883175b93bf
Shamira Naznin <shaminzn@gmail.com>
2020-03-09 18:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var kittens = [] //define your array here
+var kittens = [
+  "Milo",
+  "Otis",
+  "Garfield"
+] //define your array here
 
 // Add your functions and code here
+fucntion destructivelyAppendKitten(name) {
+  return kittens.push("kitten");
+}
